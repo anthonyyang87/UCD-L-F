d96eedefa86c9856855a4cfc396da287b9754c3e
Glitch (hello-webpage) <none>
2020-06-01 22:19:02 +0000

    🏎💄 Checkpoint ./public/screen03.html:14169005/770 ./public/screen04.html:14169005/517 ./public/script.js:14169005/1839

--- public/script.js
+++ public/script.js
@@ -40,3 +40,36 @@ function next(){
 function sendToServer(jsonObj){
 	
 }
+
+function finderNext(){
+  
+  //read from user input
+  var LostOrFound = document.getElementById('LostOrFound').value; 
+	var title = document.getElementById('title').value; 
+	var description = document.getElementById('description').value; 
+	var category = document.getElementById('category').value; 
+	var photoURL = document.getElementById('photoURL').value; 
+  
+  //store in session storage
+  sessionStorage.setItem('LostOrFound', LostOrFound); 
+  sessionStorage.setItem('title', title); 
+  sessionStorage.setItem('description', description); 
+  sessionStorage.setItem('category', category); 
+  sessionStorage.setItem('photoURL', photoURL); 
+  
+}
+
+function finderSubmit(){
+  var location = document.getElementById('location').value; 
+	var date = document.getElementById('date').value; 
+	var time = document.getElementById('time').value; 
+  
+  //maybe do not need this
+  /*
+  sessionStorage.setItem('location', location); 
+  sessionStorage.setItem('date', date);
+  sessionStorage.setItem('time', time);
+  */
+  
+  //grab previous saved values fr
+}
