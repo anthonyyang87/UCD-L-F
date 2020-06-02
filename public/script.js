0b1183b68602ba6748e660cf3c2ad0e287378bd2
Glitch (cool-ant-eth) <none>
2020-06-02 23:49:08 +0000

    Rewound to commit a48a76006aa20f1d408fc2202a5512f268017773 + 0 edits

--- public/script.js
+++ public/script.js
@@ -1,18 +1,61 @@
-function finderNext(){
-  
-  //read from user input
-  //var LostOrFound = document.getElementById('LostOrFound').value; 
+
+function next(){
+
+	/*
+	if (typeof(Storage) != "undefined") {
+		sessionStorage.setItem("LostOrFound", )
+	}
+	*/
+
+	//reading user input
+	var LostOrFound = document.getElementById('LostOrFound').value; 
 	var title = document.getElementById('title').value; 
 	var description = document.getElementById('description').value; 
 	var category = document.getElementById('category').value; 
-	var photoName = document.getElementById('imgUpload').files[0].name; 
+	var photoURL = document.getElementById('photoURL').value; 
+	var location = document.getElementById('location').value; 
+	var date = document.getElementById('date').value; 
+	var time = document.getElementById('time').value; 
+
+	
+	console.log("Input value from user input:", LostOrFound, title, description, category, photoURL, location, date, time); 
+	
+	//constructing json object
+	var jsonObj = {
+		"LostOrFound": LostOrFound, 
+		"title": title, 
+		"description": description, 
+		"category": category,
+		"photoURL": photoURL, 
+		"location": location, 
+		"date": date, 
+		"time": time
+
+	}
+
+	console.log("Json object: ", jsonObj); 
+
+
+}
+
+
+function finderNext(){
+  
+  //read from user input
+  var LostOrFound = document.getElementById('LostOrFound').value; 
+	var title = document.getElementById('title').value; 
+	var description = document.getElementById('description').value; 
+	var category = document.getElementById('category').value; 
+	var photoURL = document.getElementById('imgUpload').files[0].name; 
+  var photoData = document.getElementById('imgUpload').files[0]; 
   
   //store in session storage
-  //sessionStorage.setItem('LostOrFound', LostOrFound); 
+  sessionStorage.setItem('LostOrFound', LostOrFound); 
   sessionStorage.setItem('title', title); 
   sessionStorage.setItem('description', description); 
   sessionStorage.setItem('category', category); 
-  sessionStorage.setItem('photoName', photoName); 
+  sessionStorage.setItem('photoURL', photoURL); 
+  sessionStorage.setItem('photoData', photoData); 
   
   //experiment handle upload photo here
   var selectedFile = document.getElementById('imgUpload').files[0];
@@ -27,17 +70,16 @@ function finderNext(){
   xhr.onloadend = function(e) {
         // Get the server's response to the upload
         console.log(xhr.responseText);
-        //let newImage = document.querySelector("#cardImg");
-        //newImage.src = "../images/"+selectedFile.name;
+        let newImage = document.querySelector("#cardImg");
+        newImage.src = "../images/"+selectedFile.name;
         //console.log("Filename: ", selectedFile.name); 
-        //newImage.style.display = 'block';
-        //document.querySelector('.image').classList.remove('upload');
+        newImage.style.display = 'block';
+        document.querySelector('.image').classList.remove('upload');
       
         //Here trigger upload to media storage
         sendGetRequest(); 
-        console.log("Input data: ", sessionStorage.getItem("photoName")); 
         
-        //window.location.href = "screen04.html";
+        window.location.href = "screen04.html";
     }
   
     // actually send the request
@@ -48,7 +90,6 @@ function finderNext(){
   
 }
 
-//
 function finderSubmit(){
   var location = document.getElementById('location').value; 
 	var date = document.getElementById('date').value; 
@@ -56,22 +97,24 @@ function finderSubmit(){
   
 
   //grab saved values in previous page
-  //var LostOrFound = sessionStorage.getItem('LostOrFound'); 
+  var LostOrFound = sessionStorage.getItem('LostOrFound'); 
   var title = sessionStorage.getItem('title'); 
   var description = sessionStorage.getItem('description'); 
   var category = sessionStorage.getItem('category'); 
-  var photoName = sessionStorage.getItem('photoName'); 
+  var photoURL = sessionStorage.getItem('photoURL'); 
+  var photoData = sessionStorage.getItem('photoData'); 
   
   //constructing json object
 	var jsonObj = {
-		//"LostOrFound": LostOrFound, 
+		"LostOrFound": LostOrFound, 
 		"title": title, 
 		"description": description, 
 		"category": category,
-		"photoName": photoName, 
+		"photoURL": photoURL, 
 		"location": location, 
 		"date": date, 
 		"time": time
+
 	}
   
   //for testing if user input is collected properly
@@ -80,9 +123,10 @@ function finderSubmit(){
   //now send user data to server
   sendToServer(jsonObj); 
   
+  //uploading image to server
+  //uploadImage(photoData); 
 }
 
-//sends input data to server
 function sendToServer(data){
   
   // new HttpRequest instance 
@@ -103,6 +147,63 @@ function sendToServer(data){
 }
 
 
+function uploadImage(data){
+  //store in formData
+  const formData = new FormData(); 
+  console.log("Data Type: ", typeof data); 
+  console.log("Photo Data: ", data); 
+  var selectedFile = data;  
+  formData.append('newImage', selectedFile, selectedFile.name); 
+  //formData.append('newImage', selectedFile); 
+  
+  //build http request data structure
+  const xhr = new XMLHttpRequest(); 
+  xhr.open("POST", "/upload", true); 
+  xhr.onloadend = function(e) {
+        // Get the server's response to the upload
+        console.log(xhr.responseText);
+        let newImage = document.querySelector("#cardImg");
+        newImage.src = "../images/"+selectedFile.name;
+        //console.log("Filename: ", selectedFile.name); 
+        newImage.style.display = 'block';
+        document.querySelector('.image').classList.remove('upload');
+      
+        //Here trigger upload to media storage
+        sendGetRequest(); 
+    }
+  
+    // actually send the request
+    xhr.send(formData);
+}
+
+function uploadImage2(){
+  //store in formData
+  var selectedFile = document.getElementById("imgUpload").files[0]; 
+  const formData = new FormData(); 
+  //var selectedFile = data;  
+  formData.append('newImage', selectedFile, selectedFile.name); 
+  //formData.append('newImage', selectedFile); 
+  
+  //build http request data structure
+  const xhr = new XMLHttpRequest(); 
+  xhr.open("POST", "/upload", true); 
+  xhr.onloadend = function(e) {
+        // Get the server's response to the upload
+        console.log(xhr.responseText);
+        let newImage = document.querySelector("#cardImg");
+        newImage.src = "../images/"+selectedFile.name;
+        //console.log("Filename: ", selectedFile.name); 
+        newImage.style.display = 'block';
+        document.querySelector('.image').classList.remove('upload');
+      
+        //Here trigger upload to media storage
+        sendGetRequest(); 
+    }
+  
+    // actually send the request
+    xhr.send(formData);
+}
+
 // sends an AJAX request asking the server 
 function sendGetRequest() {
   let xhr = new XMLHttpRequest;
@@ -121,3 +222,16 @@ function sendGetRequest() {
   // Actually send request to server
   xhr.send();
 }
+
+//THE FOLLOWING TWO FUNCTIONS ARE USED FOR MEDIA STORAGE
+/*
+function showMsg(elmtId, returnedText, otherOne) {
+        let msg = document.getElementById(elmtId);
+        msg.textContent = msg.textContent.trim()+returnedText;
+        msg.className = "visible";
+        
+        // you can only push the button once
+        let uploadButton = document.getElementById("sendRequest");
+        uploadButton.removeEventListener("click", sendGetRequest);
+}
+*/
--- server.js
+++ server.js
@@ -1,273 +1,217 @@
-// We need many modules  
+// server.js
+// where your node app starts
 
-// some of the ones we have used before
+// include modules
 const express = require('express');
+
+const multer = require('multer');
 const bodyParser = require('body-parser');
-// const assets = require('./assets');
-// const sqlite3 = require('sqlite3');  // we'll need this later
+const fs = require('fs');
+const sql = require("sqlite3").verbose();
+const FormData = require("form-data");
 
-// and some new ones related to doing the login process
-const passport = require('passport');
-// There are other strategies, including Facebook and Spotify
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+let filename = ""; 
+let filename2 = ""; 
+//creating database if not exist
+const lostDB = new sql.Database("LostAndFoundTable.db"); 
 
-// Some modules related to cookies, which indicate that the user
-// is logged in
-const cookieParser = require('cookie-parser');
-const expressSession = require('express-session');
+let cmd = " SELECT name FROM sqlite_master WHERE type='table' AND name='LostAndFoundTable' "; 
+lostDB.get(cmd, function(err, val){
+  console.log("Error here:", err, val); 
+  if (val == undefined) {
+    console.log("Database not found -- Creating one now..."); 
+    createDB(); 
+  }
+  else {
+    console.log("Database found"); 
+  }
+}); 
 
-const request = require('request')
+//the function that creates database
+function createDB() {
+	const cmd = " CREATE TABLE LostAndFoundTable ()"; 
+}
 
-// let dbRowId = 1;
-
-// Setup passport, passing it information about what we want to do
-passport.use(new GoogleStrategy(
-  // object containing data to be sent to Google to kick off the login process
-  // the process.env values come from the key.env file of your app
-  // They won't be found unless you have put in a client ID and secret for 
-  // the project you set up at Google
-  {
-  clientID: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET,
-  // CHANGE THE FOLLOWING LINE TO USE THE NAME OF YOUR APP
-  callbackURL: 'https://cool-ant-eth.glitch.me/auth/accepted',  
-  userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo', // where to go for info
-  scope: ['profile', 'email']  // the information we will ask for from Google
-},
-  // function to call to once login is accomplished, to get info about user from Google;
-  // it is defined down below.
-  gotProfile));
-
-
-// Start setting up the Server pipeline
+// begin constructing the server pipeline
 const app = express();
-console.log("setting up pipeline")
 
-// take HTTP message body and put it as a string into req.body
-app.use(bodyParser.urlencoded({extended: true}));
-
-// puts cookies into req.cookies
-app.use(cookieParser());
-
-// pipeline stage that echos the url and shows the cookies, for debugging.
-app.use("/", printIncomingRequest);
-
-// Now some stages that decrypt and use cookies
-
-// express handles decryption of cooikes, storage of data about the session, 
-// and deletes cookies when they expire
-app.use(expressSession(
-  { 
-    secret:'bananaBread',  // a random string used for encryption of cookies
-    maxAge: 6 * 60 * 60 * 1000, // Cookie time out - six hours in milliseconds
-    // setting these to default values to prevent warning messages
-    resave: true,
-    saveUninitialized: false,
-    // make a named session cookie; makes one called "connect.sid" as well
-    name: "ecs162-session-cookie"
-  }));
-
-// Initializes request object for further handling by passport
-app.use(passport.initialize()); 
-
-// If there is a valid cookie, will call passport.deserializeUser()
-// which is defined below.  We can use this to get user data out of
-// a user database table, if we make one.
-// Does nothing if there is no cookie
-app.use(passport.session()); 
+// Serve static files out of public directory
+app.use(express.static('public'));
+// The body-parser is used on requests with application/json in header
+// parses the JSON in the HTTP request body, and puts the resulting object 
+// into request.body
+app.use(bodyParser.json()); 
 
 
+//here handles GET requests from client
+function handlePostcard(req, res, next){
+  //let url = "g"; 
+  let key = req.query.id; 
+  let cmd = "SELECT randomString, jsonString FROM PostCardTable WHERE randomString=?";
+  lostDB.all(cmd, key, function (err, data){
+    if (err){
+      console.log("Database reading error:", err.message); 
+    } else {
+      res.json(data[0]["jsonString"]); 
+      console.log(data[0]["jsonString"]); 
+    }
+  })
+}
 
-// The usual pipeline stages
-
-// Public files are still serverd as usual out of /public
-app.get('/*',express.static('public'));
-
-// special case for base URL, goes to index.html
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/public/index.html');
+// Now construct the server pipeline
+// Special case for request with just the base URL
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + "/public/index.html");
 });
 
-// Glitch assests directory 
-// app.use("/assets", assets);
-
-// stage to serve files from /user, only works if user in logged in
-
-// If user data is populated (by deserializeUser) and the
-// session cookie is present, get files out 
-// of /user using a static server. 
-// Otherwise, user is redirected to public splash page (/index) by
-// requireLogin (defined below)
-app.get('/user/*', requireUser, requireLogin, express.static('.'));
 
 
 
-
-// Now the pipeline stages that handle the login process itself
-
-// Handler for url that starts off login with Google.
-// The app (in public/index.html) links to here (note not an AJAX request!)
-// Kicks off login process by telling Browser to redirect to Google.
-app.get('/auth/google', passport.authenticate('google'));
-// The first time its called, passport.authenticate sends 302 
-// response (redirect) to the Browser
-// with fancy redirect URL that Browser will send to Google,
-// containing request for profile, and
-// using this app's client ID string to identify the app trying to log in.
-// The Browser passes this on to Google, which brings up the login screen. 
-
-
-// Google redirects here after user successfully logs in. 
-// This second call to "passport.authenticate" will issue Server's own HTTPS 
-// request to Google to access the user's profile information with the  	
-// temporary key we got from Google.
-// After that, it calls gotProfile, so we can, for instance, store the profile in 
-// a user database table. 
-// Then it will call passport.serializeUser, also defined below.
-// Then it either sends a response to Google redirecting to the 
-// /setcookie endpoint, below
-// or, if failure, it goes back to the public splash page. 
-// NOTE:  Apparently, this ends up at the failureRedirect if we
-// do the revoke in gotProfile.  So, if you want to redirect somewhere
-// else for a non-UCDavis ID, do it there. 
-app.get('/auth/accepted', 
-  passport.authenticate('google', 
-    { successRedirect: '/setcookie', failureRedirect: '/?email=notUCD'  }
-  )
-);
-
-// One more time! a cookie is set before redirecting
-// to the protected homepage
-// this route uses two middleware functions.
-// requireUser is defined below; it makes sure req.user is defined
-// the second one makes a public cookie called
-// google-passport-example
-app.get('/setcookie', requireUser,
-  function(req, res) {
-    // if(req.get('Referrer') && req.get('Referrer').indexOf("google.com")!=-1){
-      // mark the birth of this cookie
+// Handle a POST request containing JSON
+app.use(bodyParser.json());
+// gets JSON data into req.body
+app.post('/newItem', function (req, res) {
+  console.log("Server received: ", req.body);
+  // save the JSON string into database
+  let randomString = makeRandStr(8); 
+  let userData = JSON.stringify(req.body); 
+  res.send(userData); 
+  /*
+  //put new item into database
+  cmd = "INSERT INTO PostcardTable ( randomString, jsonString) VALUES (?,?)"; 
+  lostDB.run(cmd, randomString, userData, function(err){
+    if(err) {
+      console.log("Database Insert Error!"); 
+    } else {
+      let newId = this.lastID; //rowid of last inserted item
+      
+      res.send(randomString); 
+    }
+  }); 
+  */
   
-      // set a public cookie; the session cookie was already set by Passport
-      res.cookie('google-passport-example', new Date());
-      // res.redirect('/user/hello.html');
-    //} else {
-    //   res.redirect('/');
-    //}
+});
+
+let storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, __dirname+'/images')    
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
   }
-);
+})
+// let upload = multer({dest: __dirname+"/assets"});
+let upload = multer({storage: storage});
 
 
-// currently not used
-// using this route, we can clear the cookie and close the session
-app.get('/user/logoff',
-  function(req, res) {
-    // clear both the public and the named session cookie
-    res.clearCookie('google-passport-example');
-    res.clearCookie('ecs162-session-cookie');
-    res.redirect('/');
+// Also serve static files out of /images
+app.use("/images",express.static('images'));
+
+// Handle GET request to base URL with no other route specified
+// by sending creator.html, the main page of the app
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/public/index.html');
+});
+
+
+// Handle a post request to upload an image. 
+app.post('/upload', upload.single('newImage'), function (request, response) {
+  console.log("Recieved",request.file.originalname,request.file.size,"bytes")
+  filename = "/images/" + request.file.originalname; 
+  filename2 = "images/" + request.file.originalname; 
+  if(request.file) {
+    // file is automatically stored in /images, 
+    // even though we can't see it. 
+    // We set this up when configuring multer
+    response.end("recieved "+request.file.originalname);
   }
-);
+  else throw 'error';
+});
 
+//Here we integrate media storage
+// Send a fixed file for now
+//const filename = '/images/LJ.jpg';
 
+// fire off the file upload if we get this "GET"
+app.get("/sendUploadToAPI", function(request, response){
+        sendMediaStore(filename, request, response);
+});
 
+// function called when the button is pushed
+// handles the upload to the media storage API
+function sendMediaStore(filename, serverRequest, serverResponse) {
+  let apiKey = process.env.ECS162KEY;
+  if (apiKey === undefined) {
+    serverResponse.status(400);
+    serverResponse.send("No API key provided");
+  } else {
+    // we'll send the image from the server in a FormData object
+    let form = new FormData();
+    
+    // we can stick other stuff in there too, like the apiKey
+    form.append("apiKey", apiKey);
+    // stick the image into the formdata object
+    form.append("storeImage", fs.createReadStream(__dirname + filename));
+    // and send it off to this URL
+    form.submit("http://ecs162.org:3000/fileUploadToAPI", function(err, APIres) {
+      // did we get a response from the API server at all?
+      if (APIres) {
+        // OK we did
+        console.log("API response status", APIres.statusCode);
+        // the body arrives in chunks - how gruesome!
+        // this is the kind stream handling that the body-parser 
+        // module handles for us in Express.  
+        let body = "";
+        APIres.on("data", chunk => {
+          body += chunk;
+        });
+        APIres.on("end", () => {
+          // now we have the whole body
+          if (APIres.statusCode != 200) {
+            serverResponse.status(400); // bad request
+            serverResponse.send(" Media server says: " + body);
+          } else {
+            serverResponse.status(200);
+            serverResponse.send(body);
+            
+            
+            try {
+              //let filepath = "https://ant-better-post.glitch.me" + filename; 
+              fs.unlink(filename2); 
+            } catch(err) {
+              console.error(err); 
+            }
+            
+          }
+        });
+      } else { // didn't get APIres at all
+        serverResponse.status(500); // internal server error
+        serverResponse.send("Media server seems to be down.");
+      }
+    });
+  }
+}
+
+//generating random string
+function makeRandStr(length) {
+   var result           = '';
+   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   var charactersLength = characters.length;
+   for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+   return result;
+}
+
+// The GET AJAX query is handled by the static server, since the 
+// file postcardData.json is stored in /public
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function() {
+var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
 
-// Some functions called by the handlers in the pipeline above
 
 
-// Function for debugging. Just prints the incoming URL, and calls next.
-// Never sends response back. 
-function printIncomingRequest (req, res, next) {
-    // console.log("Serving",req.url);
-    if (req.cookies) {
-      // console.log("cookies",req.cookies)
-    }
-    next();
-}
-
-// function that handles response from Google containint the profiles information. 
-// It is called by Passport after the second time passport.authenticate
-// is called (in /auth/accepted/)
-function gotProfile(accessToken, refreshToken, profile, done) {
-    console.log("Google profile",profile);
-    // here is a good place to check if user is in DB,
-    // and to store him in DB if not already there. 
-    // Second arg to "done" will be passed into serializeUser,
-    // should be key to get user out of database.
-  
-    // Check if email is a UCD email
-    var emailAddress = profile['emails'][0]['value'];
-    console.log('user email: ', emailAddress);
-
-    let dbRowID = 1;  // temporary! Should be the real unique
-    // key for db Row for this user in DB table.
-    // Note: cannot be zero, has to be something that evaluates to
-    // True.  
-  
-    if (!emailAddress.includes('ucdavis.edu')) {
-      // Indicate user did NOT login with a UCD email
-      dbRowID = -1;
-      console.log("Non UCD email!");
-      request.get('https://accounts.google.com/o/oauth2/revoke', {
-        qs:{token: accessToken }},  function (err, res, body) {
-        console.log("revoked token");
-      }) 
-    }
-
-    done(null, dbRowID); 
-}
-
-
-// Part of Server's sesssion set-up.  
-// The second operand of "done" becomes the input to deserializeUser
-// on every subsequent HTTP request with this session's cookie. 
-// For instance, if there was some specific profile information, or
-// some user history with this Website we pull out of the user table
-// using dbRowID.  But for now we'll just pass out the dbRowID itself.
-passport.serializeUser((dbRowID, done) => {
-    console.log("SerializeUser. Input is",dbRowID);
-    done(null, dbRowID);
-});
-
-// Called by passport.session pipeline stage on every HTTP request with
-// a current session cookie (so, while user is logged in)
-// This time, 
-// whatever we pass in the "done" callback goes into the req.user property
-// and can be grabbed from there by other middleware functions
-passport.deserializeUser((dbRowID, done) => {
-    console.log("deserializeUser. Input is:", dbRowID);
-    // here is a good place to look up user data in database using
-    // dbRowID. Put whatever you want into an object. It ends up
-    // as the property "user" of the "req" object. 
-    let userData = dbRowID;
-    done(null, userData);
-});
-
-function requireUser (req, res, next) {
-  console.log("require user",req.user)
-  if (!req.user) {
-    res.redirect('/');
-  } else {
-    if(req.user == -1) {
-      res.redirect('/screen01.html?email=notUCD');
-    } else {
-      res.redirect('/screen02.html');
-    }
-    // next();
-  }
-};
-
-function requireLogin (req, res, next) {
-  console.log("checking:",req.cookies);
-  if (!req.cookies['ecs162-session-cookie']) {
-    res.redirect('/');
-  } else {
-    next();
-  }
-};
 
