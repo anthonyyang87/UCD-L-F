deacd854849e1a3eb1aff58b460ef6ea91450c8e
Glitch (hello-webpage) <none>
2020-06-02 01:27:49 +0000

    📡☂️ Checkpoint ./public/script.js:14169005/1737

--- public/script.js
+++ public/script.js
@@ -95,7 +95,7 @@ function finderSubmit(){
   sendToServer(jsonObj); 
   
   //uploading image to server
-  uploadImage(); 
+  uploadImage(photoData); 
 }
 
 function sendToServer(data){
@@ -117,12 +117,49 @@ function sendToServer(data){
   xmlhttp.send(JSON.stringify(data));
 }
 
-window.onload = function(){
-  document.getElementById('imgUpload').addEventListener('click', uploadImage()); 
+
+function uploadImage(selectedFile){
+  //store in formData
+  const formData = new FormData(); 
+  formData.append('newImage', selectedFile, selectedFile.name); 
+  
+  //build http request data structure
+  const xhr = new XMLHttpRequest(); 
+  xhr.open("POST", "/upload", true); 
+  xhr.onloadend = function(e) {
+        // Get the server's response to the upload
+        console.log(xhr.responseText);
+        let newImage = document.querySelector("#cardImg");
+        newImage.src = "../images/"+selectedFile.name;
+        //console.log("Filename: ", selectedFile.name); 
+        newImage.style.display = 'block';
+        document.querySelector('.image').classList.remove('upload');
+      
+        //Here trigger upload to media storage
+        sendGetRequest(); 
+    }
+  
+    // actually send the request
+    xhr.send(formData);
 }
 
-function uploadImage(){
+// sends an AJAX request asking the server 
+function sendGetRequest() {
+  let xhr = new XMLHttpRequest;
+  // it's a GET request, it goes to URL /seneUploadToAPI
+  xhr.open("GET","sendUploadToAPI");
   
+  // Add an event listener for when the HTTP response is loaded
+  xhr.addEventListener("load", function() {
+      if (xhr.status == 200) {  // success
+        console.log("Image uploaded to media storage.");
+      } else { // failure
+        console.log("Upload to media storage failed!"); 
+      }
+  });
+  
+  // Actually send request to server
+  xhr.send();
 }
 
 //THE FOLLOWING TWO FUNCTIONS ARE USED FOR MEDIA STORAGE
